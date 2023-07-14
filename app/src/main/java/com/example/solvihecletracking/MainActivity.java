a0d3eb5b72ea5e8a0c7cbc2f17ebc2c8326c25d2
sonyang022 <sonyang022@student.wethinkcode.co.za>
2023-07-14 13:43:33 +0200

    main activity

--- app/src/main/java/com/example/solvihecletracking/MainActivity.java
+++ app/src/main/java/com/example/solvihecletracking/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.solvihecletracking;
 
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView roadMap,carImage;
+    TextView text1,text2;
+    Animation topAnim,bottomAnim,leftAnim;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screens);
+        roadMap = findViewById(R.id.roadmap);
+        carImage = findViewById(R.id.car);
+        text1 = findViewById(R.id.text1);
+        text2 = findViewById(R.id.text2);
+
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.topanim);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottomanim);
+        leftAnim = AnimationUtils.loadAnimation(this,R.anim.leftanim);
+
+        roadMap.setAnimation(topAnim);
+        carImage.setAnimation(leftAnim);
+        text1.setAnimation(bottomAnim);
+        text2.setAnimation(bottomAnim);
+
     }
 }
